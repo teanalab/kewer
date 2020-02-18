98ba49ff69681888429f25d5552e52a75510741d
Fedor Nikolaev <fedor@wayne.edu>
2020-02-18 18:28:21 -0500

    Script to create 'indexed' file

--- make-indexed.sh
+++ make-indexed.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
-grep -h ' <http://www.w3.org/2000/01/rdf-schema#comment> ' ./dbpedia-2015-10-nolinks/* | cut -f1 -d' ' | sort > /tmp/commented
-grep -h ' <http://www.w3.org/2000/01/rdf-schema#label> ' ./dbpedia-2015-10-nolinks/* | cut -f1 -d' ' | sort > /tmp/labelled
-comm -12 /tmp/commented /tmp/labelled > indexed
+commented=$(mktemp)
+labeled=$(mktemp)
+grep -h ' <http://www.w3.org/2000/01/rdf-schema#comment> ' ./dbpedia-2015-10-kewer/short_abstracts_en.ttl | cut -f1 -d' ' | sort > "$commented"
+grep -h ' <http://www.w3.org/2000/01/rdf-schema#label> ' ./dbpedia-2015-10-kewer/labels/labels_en.ttl | cut -f1 -d' ' | sort > "$labeled"
+comm -12 "$commented" "$labeled" > indexed
+rm "$commented" "$labeled"
