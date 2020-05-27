bdcbfe661ab6d5e11be92258140b3ff1a869eaa2
Fedor Nikolaev <fedor@wayne.edu>
2020-05-27 16:41:08 -0400

    Remove credentials from smaph-responses.py

--- entity-extraction/smaph/smaph-responses.py
+++ entity-extraction/smaph/smaph-responses.py
@@ -13,9 +13,10 @@ url = "https://smaph.d4science.org/smaph/annotate"
 headers = {'accept': 'application/json'}
 responses = {}
 
-cse_id = '000629709149742239249:wqavhxaeqxc'
-google_key = 'AIzaSyBDBbPQY-WhicE1s3UVYYYDR28BmT27IJw'
-smaph_token = 'b8980005-744a-4b2f-b39d-5e6e8e1160fc-843339462'
+# Follow instructions on https://github.com/marcocor/smaph to obtain below credentials
+cse_id = ''
+google_key = ''
+smaph_token = ''
 
 with open('../queries-v2.txt') as qf:
     for line in qf:
